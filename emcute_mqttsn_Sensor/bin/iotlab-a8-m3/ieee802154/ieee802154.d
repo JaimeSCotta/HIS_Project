b73a79f236e97dcca3aaee561d51b0f419f5fba2
jaime <jaime.scotta@alumnos.upm.es>
2023-01-24 16:30:11 +0100

    Configure the Makefile of mqtt_Sensors and mqtt_RIOTwithSensors

---
+++ emcute_mqttsn_Sensor/bin/iotlab-a8-m3/ieee802154/ieee802154.d
@@ -0,0 +1,50 @@
+/home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn_Sensor/bin/iotlab-a8-m3/ieee802154/ieee802154.o: \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/sys/net/link_layer/ieee802154/ieee802154.c \
+ /home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn_Sensor/bin/iotlab-a8-m3/riotbuild/riotbuild.h \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/core/lib/include/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/include/newlib/errno.h /usr/include/newlib/sys/errno.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h /usr/include/newlib/string.h \
+ /usr/include/newlib/_ansi.h /usr/include/newlib/sys/cdefs.h \
+ /usr/include/newlib/sys/string.h \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/net/ieee802154.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/byteorder.h \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/unaligned.h \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/net/eui64.h \
+ /home/jaime-iot/Desktop/HIS_Project/RIOT/core/lib/include/modules.h
+/home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn_Sensor/bin/iotlab-a8-m3/riotbuild/riotbuild.h:
+/home/jaime-iot/Desktop/HIS_Project/RIOT/core/lib/include/assert.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/usr/include/newlib/errno.h:
+/usr/include/newlib/sys/errno.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/lock.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/sys/string.h:
+/home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/net/ieee802154.h:
+/usr/include/newlib/stdlib.h:
+/usr/include/newlib/machine/stdlib.h:
+/home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/byteorder.h:
+/home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/unaligned.h:
+/home/jaime-iot/Desktop/HIS_Project/RIOT/sys/include/net/eui64.h:
+/home/jaime-iot/Desktop/HIS_Project/RIOT/core/lib/include/modules.h:
