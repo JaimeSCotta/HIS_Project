d77eefcdfd57be7856f7f4293329fc625d95bb47
jaime <jaime.scotta@alumnos.upm.es>
2023-01-30 14:27:13 +0100

    zig zag emcute

---
+++ emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/gnrc_udp/gnrc_udp.d
@@ -0,0 +1,257 @@
+/home/jaime-iot/Desktop/attemptsSensorsCode/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/gnrc_udp/gnrc_udp.o: \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/net/gnrc/transport_layer/udp/gnrc_udp.c \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/assert.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /usr/include/newlib/errno.h /usr/include/newlib/sys/errno.h \
+ /usr/include/newlib/sys/reent.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/nano/newlib.h /usr/include/newlib/_newlib_version.h \
+ /usr/include/newlib/sys/config.h /usr/include/newlib/machine/ieeefp.h \
+ /usr/include/newlib/sys/features.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /usr/include/newlib/sys/_types.h /usr/include/newlib/machine/_types.h \
+ /usr/include/newlib/machine/_default_types.h \
+ /usr/include/newlib/sys/lock.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/byteorder.h \
+ /usr/include/newlib/string.h /usr/include/newlib/_ansi.h \
+ /usr/include/newlib/sys/cdefs.h /usr/include/newlib/sys/string.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/unaligned.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/msg.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/sched.h \
+ /usr/include/newlib/inttypes.h /usr/include/newlib/sys/_intsup.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/kernel_defines.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/modules.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/riot_version.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/compiler_hints.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/container.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/native_sched.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/clist.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/list.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/cib.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/assert.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/msg.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/stm32/include/cpu_conf.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/cpu_conf_common.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/build/stm32/cmsis/f1/Include/stm32f1xx.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/build/stm32/cmsis/f1/Include/stm32f103xe.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/core_cm3.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/cmsis_version.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/cmsis_compiler.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/cmsis_gcc.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/build/stm32/cmsis/f1/Include/system_stm32f1xx.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/stm32/include/irqs/f1/irqs.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/thread_config.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/sched.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread_flags.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/thread_arch.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/utlist.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/hdr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/inet_csum.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/addr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv4/addr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/udp.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/netopt.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netapi.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/nettype.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/modules.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ethertype.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/protnum.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/pkt.h \
+ /usr/include/newlib/stdlib.h /usr/include/newlib/machine/stdlib.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/list.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netreg.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/mbox.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/event.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/irq.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/irq_arch.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/debug_irq_disable.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread_flags.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/ptrtag.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/ztimer.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/mutex.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/rmutex.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/conf.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/dhcpv6/client.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ieee802154.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/eui64.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ethernet/hdr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/conf.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/6lo.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/flags.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/ipv6.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/evtimer_msg.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/evtimer.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/timex.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/time_units.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/l2util.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ndp.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/drivers/include/net/netdev.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/iolist.h \
+ /usr/include/newlib/unistd.h /usr/include/newlib/sys/unistd.h \
+ /usr/include/newlib/sys/types.h /usr/include/newlib/sys/_stdint.h \
+ /usr/include/newlib/sys/_pthreadtypes.h \
+ /usr/include/newlib/machine/types.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/netif.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/ext.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/ext/frag.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/ext/rh.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/hdr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/internal.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/pktbuf.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/neterr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/udp.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/icmpv6/error.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/icmpv6.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/ext.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/timex.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/hdr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/abr.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/evtimer.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/ft.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/nc.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/pl.h \
+ /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/debug.h \
+ /usr/include/newlib/stdio.h \
+ /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /usr/include/newlib/sys/stdio.h
+/home/jaime-iot/Desktop/attemptsSensorsCode/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/assert.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/usr/include/newlib/errno.h:
+/usr/include/newlib/sys/errno.h:
+/usr/include/newlib/sys/reent.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/nano/newlib.h:
+/usr/include/newlib/_newlib_version.h:
+/usr/include/newlib/sys/config.h:
+/usr/include/newlib/machine/ieeefp.h:
+/usr/include/newlib/sys/features.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/usr/include/newlib/sys/_types.h:
+/usr/include/newlib/machine/_types.h:
+/usr/include/newlib/machine/_default_types.h:
+/usr/include/newlib/sys/lock.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/byteorder.h:
+/usr/include/newlib/string.h:
+/usr/include/newlib/_ansi.h:
+/usr/include/newlib/sys/cdefs.h:
+/usr/include/newlib/sys/string.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/unaligned.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/msg.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/sched.h:
+/usr/include/newlib/inttypes.h:
+/usr/include/newlib/sys/_intsup.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/kernel_defines.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/modules.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/riot_version.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/compiler_hints.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/container.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/native_sched.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/clist.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/list.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/cib.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/assert.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/msg.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/stm32/include/cpu_conf.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/cpu_conf_common.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/build/stm32/cmsis/f1/Include/stm32f1xx.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/build/stm32/cmsis/f1/Include/stm32f103xe.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/core_cm3.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/cmsis_version.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/cmsis_compiler.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/vendor/cmsis_gcc.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/build/stm32/cmsis/f1/Include/system_stm32f1xx.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/stm32/include/irqs/f1/irqs.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/thread_config.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/sched.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread_flags.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/thread_arch.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/utlist.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/hdr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/inet_csum.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/addr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv4/addr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/udp.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/netopt.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netapi.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/nettype.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/modules.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ethertype.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/protnum.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/pkt.h:
+/usr/include/newlib/stdlib.h:
+/usr/include/newlib/machine/stdlib.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/list.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netreg.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/mbox.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/event.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/irq.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/cpu/cortexm_common/include/irq_arch.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/debug_irq_disable.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread_flags.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/ptrtag.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/ztimer.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/mutex.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/include/thread.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/rmutex.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/conf.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/dhcpv6/client.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ieee802154.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/eui64.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ethernet/hdr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/conf.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/6lo.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/flags.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/ipv6.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/evtimer_msg.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/evtimer.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/timex.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/time_units.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/l2util.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ndp.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/drivers/include/net/netdev.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/iolist.h:
+/usr/include/newlib/unistd.h:
+/usr/include/newlib/sys/unistd.h:
+/usr/include/newlib/sys/types.h:
+/usr/include/newlib/sys/_stdint.h:
+/usr/include/newlib/sys/_pthreadtypes.h:
+/usr/include/newlib/machine/types.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/netif.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/ext.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/ext/frag.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/ipv6/ext/rh.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/hdr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/netif/internal.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/pktbuf.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/neterr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/udp.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/icmpv6/error.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/icmpv6.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/ext.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/timex.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/hdr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/abr.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/evtimer.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/ft.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/nc.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/include/net/gnrc/ipv6/nib/pl.h:
+/home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/core/lib/include/debug.h:
+/usr/include/newlib/stdio.h:
+/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h:
+/usr/include/newlib/sys/stdio.h:
