00d3d0dcedc87a8b294b21cea5cf1fcf4f2eef50
jaime <jaime.scotta@alumnos.upm.es>
2023-01-31 09:33:46 +0100

    Port 13 to the .elf updated

--- emcute_mqttsn/bin/native/cpu/tramp.d
+++
@@ -1,6 +0,0 @@
-/home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn/bin/native/cpu/tramp.o: \
- /home/jaime-iot/Desktop/HIS_Project/RIOT/cpu/native/tramp.S \
- /usr/include/stdc-predef.h \
- /home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn/bin/native/riotbuild/riotbuild.h
-/usr/include/stdc-predef.h:
-/home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn/bin/native/riotbuild/riotbuild.h:
--- emcute_mqttsn/sensordefinition.h
+++
@@ -1,8 +0,0 @@
-// struct that contains sensors
-typedef struct sensors{
-  int temperature;
-  int humidity;
-  int windDirection;
-  int windIntensity;
-  int rainHeight;
-}t_sensors;
