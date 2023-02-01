674279916482704b15a20f42bca39e55d43c7d26
tranhocphuc <phuctranhoc@gmail.com>
2023-02-01 11:26:46 +0100

    Update new .elf file

--- emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/random/musl_lcg.d
+++ emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/random/musl_lcg.d
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /home/trnhphc/Desktop/HIS_Project/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/random/musl_lcg.o: \
  /home/trnhphc/Desktop/HIS_Project/RIOT/sys/random/musl_lcg.c \
  /home/trnhphc/Desktop/HIS_Project/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h \
@@ -17,18 +16,3 @@
 /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h:
 /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h:
 /usr/share/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h:
-||||||| merged common ancestors
-/home/jaime-iot/Desktop/attemptsSensorsCode/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/random/musl_lcg.o: \
- /home/jaime-iot/Desktop/attemptsSensorsCode/RIOT/sys/random/musl_lcg.c \
- /home/jaime-iot/Desktop/attemptsSensorsCode/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h
-/home/jaime-iot/Desktop/attemptsSensorsCode/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h:
-/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
-=======
-/home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/random/musl_lcg.o: \
- /home/jaime-iot/Desktop/HIS_Project/RIOT/sys/random/musl_lcg.c \
- /home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h \
- /usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h
-/home/jaime-iot/Desktop/HIS_Project/emcute_mqttsn_ZigZag/bin/iotlab-a8-m3/riotbuild/riotbuild.h:
-/usr/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
->>>>>>> 274b01a0193acbfc247ca7f7215d1a9eac6571d2
